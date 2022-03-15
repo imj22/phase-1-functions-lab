1aaec7b9cf5525d3a2663496c19277e4c1ba9e1c
imj22 <98693440+imj22@users.noreply.github.com>
2022-03-15 16:33:37 +0100

    commit tree 110571bf6bcb42f64a3f159e9ba87f9b0a4cfc83

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(number) {
+    if (number >= 42) {
+        return number - 42; 
+    } else if (number < 42) {
+        return 42 - number;
+    }
+}
+
+function distanceFromHqInFeet(number) {
+return distanceFromHqInBlocks(number)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+        return (destination-start)*264;
+    } else if (start > destination) {
+        return (start-destination)*264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+   if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) > 400, distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination)-400)*0.02;
+    } else if (distanceTravelledInFeet(start, destination) > 2000, distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25; 
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far";
+    }
+    
+
+}
